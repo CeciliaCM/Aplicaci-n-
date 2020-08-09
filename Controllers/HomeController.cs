bff81e25f377edf8a1f56ca64438bf22b9313273
CeciliaCM <65978808+CeciliaCM@users.noreply.github.com>
2020-08-09 00:40:51 -0500

    Aplicacion Web c#

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult MediasMoviles()
+        {
+            ViewBag.Message = "Modelo de medias móviles.";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult OtrosModelos()
+        {
+            ViewBag.Message = "Modelos de análisis técnico y cuantitativo";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Informacion()
+        {
+            ViewBag.Message = "Esta aplicación tiene como fin incluir modelos cuantitativos y técnicos de " +
+                "análisis del preccio de instrumentos financieros ";
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Registro()
+        {
+            ViewBag.Message = "Crea una cuenta ";
+
+
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult hasregistrado()
+        {
+            ViewBag.Message = "Te has registrado ";
+
+
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Gráfica()
+        {
+            ViewBag.Message = "Gráfica Medias Móviles ";
+
+
+
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult KIMBERA()
+        {
+            ViewBag.Message = "Gráfica Medias Móviles ";
+
+
+
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ALPEKA()
+        {
+            ViewBag.Message = "Gráfica Medias Móviles ";
+
+
+
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult TLEVISACPO()
+        {
+            ViewBag.Message = "Gráfica Medias Móviles ";
+
+
+
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CUERVO()
+        {
+            ViewBag.Message = "Gráfica Medias Móviles ";
+
+
+
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult PINFRA()
+        {
+            ViewBag.Message = "Gráfica Medias Móviles ";
+
+
+
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AccesoGráficas()
+        {
+            ViewBag.Message = "Gráfica Medias Móviles ";
+
+
+
+            return View();
+        }
+    }
+}
+
+
+        
+       
+
+
+
+
+
+ 
---
+++ Controllers/MediasMovilesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MediasMovilesController : Controller
+    {
+        // GET: MediasMoviles
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modelo de Medias Móviles ";
+
+            return View();
+
+             void Main(string[] args)
+            {
+                using (var reader = new System.IO.StreamReader(@"C:\Users\cecil\Desktop\Aplicación web\Medias Moviles"))
+                {
+                    List<string> listA = new List<string>();
+                    List<string> listB = new List<string>();
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(';');
+
+                        listA.Add(values[0]);
+                        listB.Add(values[1]);
+                    }
+                }
+            }
+
+        }
+        
+    }
+}
